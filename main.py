b090caf9ba4380f6877e0409187afec52f70c97a
ozone-one <107835405+ozone-one@users.noreply.github.com>
2024-01-30 22:02:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,43 +11,21 @@ newrelic.agent.initialize("newrelic.ini")
 @newrelic.agent.wsgi_application()
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    n = random.random()
-    if n > 0.95:
-        raise Exception("unknown exception occured")
-        newrelic.agent.notice_error()
-        return Response(
-            response=json.dumps({'error': "unknown exception happened"}),
-            mimetype='application/json',
-            status=500
-        )
-    else:
-        print("success")
-        return Response(
-            response=json.dumps({'value': 11}),
-            mimetype='application/json'
-        )
+    return Response(
+        response=json.dumps({'value': 11}),
+        mimetype='application/json'
+    )
 
 
 @newrelic.agent.wsgi_application()
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    n = random.random()
-    if n > 0.98:
-        raise Exception("unknown exception occured")
-        print("unknown exception occured")
-        newrelic.agent.notice_error()
-        return Response(
-            response=json.dumps({'error': "unknown exception happened"}),
-            mimetype='application/json',
-            status=500
-        )
-    else:
-        print("success")
-        return Response(
-            response=json.dumps(
-                {"route": "/hello", "response": "hello world main branch"}),
-            mimetype='application/json'
-        )
+    print("success")
+    return Response(
+        response=json.dumps(
+            {"route": "/hello", "response": "hello world main branch"}),
+        mimetype='application/json'
+    )
 
 
 if __name__ == "__main__":
